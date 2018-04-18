b1c628c9cb1848aaf0be7ec22d5594e5fb97fdc9
cloudy <im@joe.cloud>
2018-04-18 15:39:48 -0500

    Add model, reqs

--- ForwardModelTrain.py
+++ ForwardModelTrain.py
@@ -31,13 +31,13 @@ output_training_data=np.delete(training_data,[0,1,2,3,4,5,6],axis=1)
 forwardModel=Sequential()
 
 # Add the input Layer and the 1rst Hidden Layer
-forwardModel.add(Dense(400,input_shape=(4,),init="uniform",activation="sigmoid"))
+forwardModel.add(Dense(500,input_shape=(4,),init="uniform",activation="sigmoid"))
 
 # Add the Hidden Layer
+forwardModel.add(Dense(500,init="uniform",activation="sigmoid"))
+forwardModel.add(Dense(400,init="uniform",activation="sigmoid"))
 forwardModel.add(Dense(400,init="uniform",activation="sigmoid"))
 forwardModel.add(Dense(300,init="uniform",activation="sigmoid"))
-forwardModel.add(Dense(300,init="uniform",activation="sigmoid"))
-forwardModel.add(Dense(200,init="uniform",activation="sigmoid"))
 
 # Add the Output Layer
 forwardModel.add(Dense(3))
@@ -46,7 +46,7 @@ forwardModel.add(Dense(3))
 forwardModel.compile(optimizer='adam',loss='mse',metrics=['accuracy'])
 
 # Train the Model
-forwardModel.fit(input_training_data,output_training_data,validation_split=0.2,batch_size=1)
+forwardModel.fit(input_training_data,output_training_data,validation_split=0.2,batch_size=256, epochs=10)
 
 # Save the Model
 forwardModel.save(model_file)
